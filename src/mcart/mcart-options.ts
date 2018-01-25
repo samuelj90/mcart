53e09bfe7fce527944f34e59a481b1acea075fc9
Samuel J Mathew <samuelj90@gmail.com>
2018-01-25 16:35:22 +0530

    Fixing dependency issues and adding initial plugin option typings

--- src/mcart/mcart-options.ts
+++ src/mcart/mcart-options.ts
@@ -1,3 +1,40 @@
 export interface MCartOptions {
-   label: string;
+    productListing: ProductListingOptions;
+    minCart: MiniCartOptions;
+    cartPage: CartPageOptions;
+    confirmationPage: ConfirmationPageOptions;
+}
+
+export interface ProductListingOptions {
+    enabled: boolean;
+    appendElement?: JQuery;
+    template?: string;
+    products: Product[]
+    addToCartBtnEnabled: boolean;
+    addToCartBtnElementSelector?: string;
+    addToCartBtnClickCallbackFn?: () => void;
+    buyNowBtnEnabled: boolean;
+    buyNowBtnElementSelector?: string;
+    buyNowBtnClickCallbackFn?: () => void;
+    beforeProductListingCallbackFn?: () => void;
+    afterProductListingCallbackFn?: () => void;
+}
+
+export interface MiniCartOptions {
+    enabled: boolean;
+    template?: string;
+    appendElement?: JQuery;
+    miniCartViewCartEnabled?: boolean;
+    miniCartProceedToCheckout?: boolean;
+}
+
+export interface Product {
+
+}
+
+export interface CartPageOptions {
+
+}
+export interface ConfirmationPageOptions {
+
 }
